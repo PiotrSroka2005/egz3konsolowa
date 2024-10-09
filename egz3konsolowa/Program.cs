fb7d6bbbe972f90cc58295a8ae89608d0a72e622
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-10-09 07:41:46 +0200

    Add SprawdzPlec function

--- egz3konsolowa/Program.cs
+++ egz3konsolowa/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace egz3konsolowa
+﻿using System.ComponentModel.Design;
+
+namespace egz3konsolowa
 {
     internal class Program
     {
+
+        static char SprawdzPlec(int[] pesel)
+        {
+            char plec = ' ';
+            if (pesel[9] % 2 == 0)
+                plec = 'K';
+            else
+                plec = 'M';
+
+            return plec;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] pesel = { 5, 5, 0, 3, 0, 1, 0, 1, 1, 9, 3 };
         }
     }
 }
